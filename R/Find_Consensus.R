62573b122da34fe4c23b9aa6e4723011e8fc2b3a
Natalie Wang <83103956+WangNatalie@users.noreply.github.com>
2024-11-26 14:12:04 -0500

    Update Find_Consensus.R

--- R/Find_Consensus.R
+++ R/Find_Consensus.R
@@ -1,21 +1,25 @@
-ids_sctype <- rownames(es.max)
-ids_single2 <- rownames(pred.msc)
+Find_Consensus <- function(sctype, singler){
 
-common_ids <- intersect(ids_sctype, ids_single2)
+  ids_sctype <- rownames(sctype)
+  ids_single2 <- rownames(singler)
+  
+  common_ids <- intersect(ids_sctype, ids_single2)
+  
+  sctype_subset <- sctype[common_ids, , drop = FALSE]  
+  singler_subset <- singler[common_ids, , drop = FALSE]
+  
+  celltype_combined <- c()
+  
+  celltype_combined <- ifelse(sctype_subset$Celltype == singler_subset$pruned.labels, 
+                              sctype_subset$Celltype, "none")
+  
+  # Create a new dataframe with the matched ids and the compared Celltype values
+  consensus <- data.frame(
+    CellID = common_ids,
+    Celltype = celltype_combined,
+    sctype = sctype_subset$Celltype,
+    singler = singler_subset$pruned.labels
+  )
 
-sctype_subset <- es.max[common_ids, , drop = FALSE]  
-single2_subset <- pred.msc[common_ids, , drop = FALSE]
-
-celltype_combined <- c()
-
-celltype_combined <- ifelse(sctype_subset$Celltype == single2_subset$Celltype, 
-                            sctype_subset$Celltype, "none")
-
-# Create a new dataframe with the matched ids and the compared Celltype values
-consensus <- data.frame(
-  CellID = common_ids,
-  Celltype = celltype_combined
-)
-
-consensus <- result[rowSums(is.na(result)) != ncol(result), ] # final consensus table
-
+  consensus
+}
